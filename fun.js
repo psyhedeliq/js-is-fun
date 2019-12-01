4983fb6d0a954b6337792a5a997009c0747b50cb
psyhedeliq <filipcornelcristian@gmail.com>
2019-12-01 22:27:28 +0000

    profile lookup

--- fun.js
+++ fun.js
@@ -137,7 +137,7 @@ console.log(myArray);
 
 // ************** Recursion ************** //
 // Write a recursive function, sum(arr, n), that returns the sum of the elements from 0 to n inclusive in an array arr.
-
+/*
 const sum = (arr, n) => {
   if (n <= 0) {
     return arr[0];
@@ -147,3 +147,46 @@ const sum = (arr, n) => {
 };
 
 console.log(sum([2, 3, 4], 1));
+*/
+
+// ************** Profile Lookup ************** //
+
+const contacts = [
+  {
+    firstName: 'Akira',
+    lastName: 'Laine',
+    number: '0543236543',
+    likes: ['Pizza', 'Coding', 'Brownie Points']
+  },
+  {
+    firstName: 'Harry',
+    lastName: 'Potter',
+    number: '0994372684',
+    likes: ['Hogwarts', 'Magic', 'Hagrid']
+  },
+  {
+    firstName: 'Sherlock',
+    lastName: 'Holmes',
+    number: '0487345643',
+    likes: ['Intriguing Cases', 'Violin']
+  },
+  {
+    firstName: 'Kristian',
+    lastName: 'Vos',
+    number: 'unknown',
+    likes: ['JavaScript', 'Gaming', 'Foxes']
+  }
+];
+
+const lookupProfile = (name, prop) => {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else return 'No such property';
+    }
+  }
+  return 'No such contact';
+};
+
+console.log(lookupProfile('Harry', 'number'));
