fb90d556b4ac20c9b24d310a8955756c3c033939
psyhedeliq <filipcornelcristian@gmail.com>
2019-12-11 13:58:25 +0000

    create countdown function using recursion

--- fun.js
+++ fun.js
@@ -217,7 +217,7 @@ console.log(checkEqual(1, 2));
 */
 
 // ****** Multiple Conditional (Ternary) Operators ****** //
-
+/*
 const typeOfNum = num => {
   return num > 0 ? 'Positive' : num < 0 ? 'Negative' : 'Zero';
 };
@@ -225,3 +225,20 @@ const typeOfNum = num => {
 // console.log(typeOfNum(5));
 // console.log(typeOfNum(-5));
 console.log(typeOfNum(0));
+*/
+
+// ****** Create a countdown using recursion ****** //
+
+// We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter. If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1]. Your function must use recursion by calling itself and must not use loops of any kind.
+
+const countdown = n => {
+  if (n < 1) {
+    return [];
+  } else {
+    const countdownArr = countdown(n - 1);
+    countdownArr.unshift(n);
+    return countdownArr;
+  }
+};
+
+console.log(countdown(7)); // [7, 6, 5, 4, 3, 2, 1];
